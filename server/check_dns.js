c5e630505a1ff1eeed50021e42354bf9c36dd12d
Pavel <pav@example.com>
2026-07-08 10:15:13 +0300

    chore: clean up debug scripts, logs, and temporary files

--- server/check_dns.js
+++
@@ -1,8 +0,0 @@
-const dns = require('dns');
-dns.resolve4('api.telegram.org', (e, a) => {
-  console.log('Node v' + process.version);
-  console.log('IPv4:', a, e?.message);
-  dns.resolve6('api.telegram.org', (e2, a2) => {
-    console.log('IPv6:', a2, e2?.message);
-  });
-});
